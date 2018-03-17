80a21e1f3350c70a373a4a9d39f43cc68118ef42
jaysongacutno <32737065+jaysongacutno@users.noreply.github.com>
2018-03-17 17:40:57 +0800

    Create R_Code_4.R

---
+++ R_Code_4.R
@@ -0,0 +1,29 @@
+> setwd("/Users/jayson/Desktop/Module4")
+> hpc <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+> hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+> hpc <- subset(hpc,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+> hpc <- hpc[complete.cases(hpc),]
+> DT <- paste(hpc$Date, hpc$Time)
+> DT <- setNames(DT, "DateTime")
+> hpc <- hpc[ ,!(names(hpc) %in% c("Date","Time"))]
+> hpc <- cbind(DT, hpc)
+> hpc$DT <- as.POSIXct(DT)
+
+> with(hpc, {
++     plot(Global_active_power~DT, type="l", 
++          ylab="Global Active Power (kilowatts)", xlab="")
++     plot(Voltage~DT, type="l", 
++          ylab="Voltage", xlab="datetime")
++     plot(Sub_metering_1~DT, type="l", 
++          ylab="Energy sub metering", xlab="")
++     lines(Sub_metering_2~DT,col='Red')
++     lines(Sub_metering_3~DT,col='Blue')
++     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
++            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
++     plot(Global_reactive_power~DT, type="l", 
++          ylab="Global_reactive_power",xlab="datetime")
++ })
+
+
+> dev.copy(png,"plot4.png", width=480, height=480)
+> dev.off()
